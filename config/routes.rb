a580ca994818798f906902e21777dffcf24c4080
Hope Dorkenoo <compounddork@gmail.com>
2023-05-07 21:44:42 +0000

    work with erb and routes

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # Get
+  get "about-us", to:"about#index", as: :about
+  
+  root to: "main#index"
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
